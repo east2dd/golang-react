9ca257534c4c58efddfb999e89e061a1498bf1af
alex <xingxing2d@gmail.com>
2018-12-03 23:57:56 +1000

    [client] removed unused lib

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { getCategories } from './actions'
-
 class App extends Component {
   componentWillMount(){
     this.props.dispatch(getCategories())
